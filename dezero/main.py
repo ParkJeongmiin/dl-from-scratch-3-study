923882b87de90b1bc5b44dc0f111104bbf6a6be3
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2024-07-31 17:09:59 +0900

    FEAT : 메모리 사용량 개선 (#6)
    * feat: 불필요한 미분 결과 즉시 삭제 로직 구현
    
    #5
    
    * feat: 역전파 활성/비활성 모드 설정 로직 구현
    
    #5
    
    * feat: 역전파 모드 전환 편의성 추가
    
    #5
    
    * feat: step18 적용
    
    #5

--- dezero/main.py
+++ dezero/main.py
@@ -3,7 +3,12 @@ import numpy as np
 from core import *
 
 
-for i  in range(10):
-    x = Variable(np.random.randn(10000))        # 거대한 데이터
-    y = square(square(square(x)))               # 복잡한 계산
-    
+# with 블록 내에서는 순전파 코드만 실행된다.
+with using_config('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+# 편의함수를 이용해 간편하게 순전파 영역을 지정할 수 있다.
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
--- steps/step18.py
+++ steps/step18.py
@@ -3,9 +3,6 @@ import contextlib
 import numpy as np
 
 
-# ================================================================
-# Config
-# ================================================================
 class Config:
     enable_backprop = True
 
@@ -21,16 +18,17 @@ def using_config(name, value):
 
 
 def no_grad():
+    """
+    using_config 함수를 편하게 사용하기 위한 편의함수
+    """
     return using_config('enable_backprop', False)
 
-# ================================================================
-# Variable / Function
-# ================================================================
+
 class Variable:
     def __init__(self, data):
         if data is not None:
             if not isinstance(data, np.ndarray):
-                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+                raise TypeError('{} is not supported'.format(type(data)))
 
         self.data = data
         self.grad = None
@@ -41,20 +39,9 @@ class Variable:
         self.creator = func
         self.generation = func.generation + 1   # 세대를 기록한다.(부모 세대 + 1)
 
-    '''
-    # 재귀를 이용한 구현
-    def backward(self):
-        f = self.creator                    # 1. 함수를 가져온다.
-        if f is not None:
-            x = f.input                     # 2. 함수의 입력을 가져온다.
-            x.grad = f.backward(self.grad)  # 3. 함수의 backward 메서드를 호출한다.
-            x.backward()                    # 하나 앞 변수의 backward 메서드를 호출한다.(재귀)
-    '''
-
-    # 반복문을 이용한 backward 구현
     def backward(self, retain_grad=False):
         if self.grad is None:
-            self.grad = np.ones_like(self.data)     # main에서 y.grad를 직접 지정해주는 코드를 간소화하기 위해
+            self.grad = np.ones_like(self.data)
 
         funcs = []
         seen_set = set()
@@ -62,19 +49,19 @@ class Variable:
         def add_func(f):
             if f not in seen_set:
                 funcs.append(f)
-                seen_set.add(f)
+                seen_set.add(f)         # funcs 리스트에 같은 함수를 중복으로 추가하지 않기 위해
                 funcs.sort(key=lambda x: x.generation)
 
         add_func(self.creator)
-        
+
         while funcs:
-            f = funcs.pop()             # 함수를 가져온다.
+            f = funcs.pop()
             gys = [output().grad for output in f.outputs]
             gxs = f.backward(*gys)
             if not isinstance(gxs, tuple):
                 gxs = (gxs,)
 
-            for x, gx in zip(f.inputs, gxs):
+            for x, gx in zip(f.inputs, gxs):        # f.inputs[i]와 gxs[i] 서로 대응 관계에 있다.
                 if x.grad is None:
                     x.grad = gx
                 else:
@@ -85,18 +72,13 @@ class Variable:
             
             if not retain_grad:
                 for y in f.outputs:
-                    y().grad = None             # y는 약한 참조(weakref), 중간 변수의 미분값을 모두 None으로 설정
+                    y().grad = None     # y는 약한 참조(weakref), 중간 변수의 미분값을 모두 None으로 설정
 
     def cleargrad(self):
         self.grad = None
 
 
 def as_array(x):
-    '''
-    Variable 클래스에서 0차원의 ndarray 인스턴스를 제곱하게되면 numpy.float64 형태로 변환된다.
-    numpy.ndarray 형태로 변환 시켜줘야 한다.
-    그래서 Function 클래스에서 순전파 결과를 Variable 클래스로 초기화하기 전에 as_array로 확인 + 변환을 진행한다.
-    '''
     if np.isscalar(x):
         return np.array(x)
     return x
@@ -121,16 +103,19 @@ class Function:
 
         return outputs if len(outputs) > 1 else outputs[0]
     
-    def forward(self, in_data):
-        raise NotImplementedError()
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
     
     def backward(self, gy):
-        raise NotImplementedError()
+        return gy, gy
+
+def add(x0, x1):
+    return Add()(x0, x1)
 
 
-# ================================================================
-# 연산자 오버로드
-# ================================================================
 class Square(Function):
     def forward(self, x):
         y = x ** 2
@@ -140,35 +125,18 @@ class Square(Function):
         x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
-    
 
 def square(x):
     return Square()(x)
 
 
-class Exp(Function):
-    def forward(self, x):
-        y = np.exp(x)
-        return y
-    
-    def backward(self, gy):
-        x = self.input.data
-        gx = np.exp(x) * gy
-        return gx
-    
+# with 블록 내에서는 순전파 코드만 실행된다.
+with using_config('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
 
-def exp(x):
-    return Exp()(x)
+# 편의함수를 이용해 간편하게 순전파 영역을 지정할 수 있다.
+with no_grad():
     
-
-class Add(Function):
-    def forward(self, x0, x1):
-        y = x0 + x1
-        return y
-    
-    def backward(self, gy):
-        return gy, gy
-    
-
-def add(x0, x1):
-    return Add()(x0, x1)
+    x = Variable(np.array(2.0))
+    y = square(x)
